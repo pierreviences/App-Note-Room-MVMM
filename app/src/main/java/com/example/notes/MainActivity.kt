50628931d6e9fe1bbd7d2aeff49ae793c6c461e8
pierreviences <imaniyazid@gmail.com>
2023-09-05 15:34:23 +0700

    feat : Add aksi pencarian dan tombol tambah

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -68,7 +68,11 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onQueryTextChange(newtext: String?): Boolean {
+                if(newtext != null){
+                    adapter.filterList(newtext)
+                }
 
+                return true
             }
 
         })
