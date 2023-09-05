a74d522b0ef6cbf98c10ca4b3364c099f6eb1923
pierreviences <imaniyazid@gmail.com>
2023-09-05 15:41:22 +0700

    feat: Menambahkan fungsi penghapusan catatan pada MainActivity saat menu pop-up diaktifkan

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -4,10 +4,13 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.LinearLayout
+import android.widget.PopupMenu
 import android.widget.SearchView
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.cardview.widget.CardView
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.notes.Adapter.NotesAdapter
@@ -16,7 +19,7 @@ import com.example.notes.Models.Note
 import com.example.notes.Models.NoteViewModel
 import com.example.notes.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NotesAdapter.NotesItemClickListener, PopupMenu.OnMenuItemClickListener{
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var database: NoteDatabase
@@ -24,7 +27,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var adapter: NotesAdapter
     lateinit var selectedNote: Note
 
-    private val updateOne = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+    private val updateNote = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
         result ->
         if(result.resultCode == Activity.RESULT_OK){
             val note = result.data?.getSerializableExtra("note") as? Note
@@ -86,5 +89,28 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+    }
+
+    override fun onItemClicked(note: Note) {
+        val intent = Intent(this@MainActivity, AddNote::class.java)
+        intent.putExtra("current_note", note)
+        updateNote.launch(intent)
+    }
+    override fun onLongItemClicked (note: Note, cardView: CardView) {
+        selectedNote = note
+        popUpDisplay(cardView)
+    }
+    private fun popUpDisplay (cardView: CardView) {
+        val popup = PopupMenu(this, cardView)
+        popup.setOnMenuItemClickListener(this@MainActivity)
+        popup.inflate(R.menu.pop_up_menu)
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        if (item?.itemId == R.id.delete_note) {
+            viewModel.deleteNote(selectedNote)
+            return true
+        }
+        return false
     }
 }
