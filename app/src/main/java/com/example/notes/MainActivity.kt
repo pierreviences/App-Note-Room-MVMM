32b31012d15bf2d7eae3416b3edd64d453a5316b
pierreviences <imaniyazid@gmail.com>
2023-09-05 15:28:55 +0700

    feat: Tambahkan NoteViewModel untuk mengelola data catatan

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -1,11 +1,59 @@
 package com.example.notes
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.notes.Adapter.NotesAdapter
+import com.example.notes.Database.NoteDatabase
+import com.example.notes.Models.Note
+import com.example.notes.Models.NoteViewModel
+import com.example.notes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var database: NoteDatabase
+    lateinit var viewModel: NoteViewModel
+    lateinit var adapter: NotesAdapter
+    lateinit var selectedNote: Note
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initUI()
+        viewModel = ViewModelProvider(this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(application).get(NoteViewModel::class.java))
+
+        viewModel.allnotes.observe(this){
+            list ->
+            list?.let{
+                adapter.updateList(list)
+            }
+        }
+
+        database = NoteDatabase.getDatabase(this)
+    }
+
+    private fun initUI(){
+        binding.recycleView.setHasFixedSize(true)
+        binding.recycleView.layoutManager = StaggeredGridLayoutManager(2, LinearLayout.VERTICAL)
+        adapter = NotesAdapter(this, this)
+        binding.recycleView.adapter = adapter
+
+        val getCount = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result ->
+            if(result.resultCode == Activity.RESULT_OK){
+                val note = result.data?.getSerializableExtra("note") as? Note
+                if(note != null){
+                    viewModel.insert
+                }
+            }
+        }
     }
 }
